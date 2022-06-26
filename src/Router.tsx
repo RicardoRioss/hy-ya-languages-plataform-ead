b246266f2788ac14ff659caf29a558dfe5998d12
Ricardo Rios <rickrios23@hotmail.com>
2022-06-26 18:00:05 -0300

    app

---
+++ src/Router.tsx
@@ -0,0 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+import { Classroom } from "./pages/Classroom";
+import { Subscribe } from "./pages/Subscribe";
+
+export function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<Subscribe />} />
+      <Route path="/classroom" element={<Classroom />} />
+      <Route path="/classroom/lesson/:slug" element={<Classroom />} />
+
+    </Routes>
+  )
+}
